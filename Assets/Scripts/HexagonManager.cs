b809ded7aa2443f2daf3ceb412e494ead4006d75
Yasir Erkam Özdemir <yasir.erkam17@gmail.com>
2021-05-25 20:10:59 +0300

    Initial commit

---
+++ Assets/Scripts/HexagonManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class HexagonManager : MonoBehaviour
+{
+    public GameObject circle;
+    private IEnumerable<KeyValuePair<Transform, float>> closest3Tranforms;
+
+    public GameObject Circle { get => circle; set => circle = value; }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CalcClosest3Points(out closest3Tranforms);
+            CreatePointObject(closest3Tranforms);
+        }
+    }
+
+    private void CreatePointObject(IEnumerable<KeyValuePair<Transform, float>> closest3points)
+    {
+        List<Vector3> positions = new List<Vector3>();
+
+        foreach (var kvp in closest3points)
+        {
+            positions.Add(kvp.Key.position);
+        }
+        Vector3 center = GetCentroid(positions) - Vector3.forward;
+
+        if (Circle.GetComponent<SpriteRenderer>().enabled == false)
+        {
+            Circle.GetComponent<SpriteRenderer>().enabled = true;
+        }
+
+        Circle.transform.position = center;
+    }
+
+    private void Rotate3Transform()
+    {
+        foreach (var item in closest3Tranforms)
+        {
+            item.Key.parent = Circle.transform;
+        }
+
+        Circle.transform.Rotate(0, 0, 15);
+
+        foreach (var item in closest3Tranforms)
+        {
+            item.Key.parent = transform;
+        }
+    }
+
+    private void CalcClosest3Points(out IEnumerable<KeyValuePair<Transform, float>> closest3points)
+    {
+        Dictionary<Transform, float> childDistance = new Dictionary<Transform, float>();
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform child = transform.GetChild(i).transform;
+            Vector2 childScrPos = Camera.main.WorldToScreenPoint(child.position);
+
+            float distance = Vector2.Distance(childScrPos, Input.mousePosition);
+
+            childDistance.Add(child, distance);
+        }
+
+        closest3points = childDistance.OrderBy(kvp => kvp.Value).Take(3);
+    }
+
+    public static Vector3 GetCentroid(List<Vector3> poly)
+    {
+        float accumulatedArea = 0.0f;
+        float centerX = 0.0f;
+        float centerY = 0.0f;
+
+        for (int i = 0, j = poly.Count - 1; i < poly.Count; j = i++)
+        {
+            float temp = poly[i].x * poly[j].y - poly[j].x * poly[i].y;
+            accumulatedArea += temp;
+            centerX += (poly[i].x + poly[j].x) * temp;
+            centerY += (poly[i].y + poly[j].y) * temp;
+        }
+
+        if (Math.Abs(accumulatedArea) < 1E-7f)
+            return new Vector3();  // Avoid division by zero
+
+        accumulatedArea *= 3f;
+        return new Vector3(centerX / accumulatedArea, centerY / accumulatedArea);
+    }
+}
---
+++ Assets/Scripts/MyGrid.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGrid : MonoBehaviour
+{
+    public event EventHandler<OnGridValueChangedEventArgs> OnGridValueChanged;
+
+    public class OnGridValueChangedEventArgs : EventArgs
+    {
+        public int x;
+        public int y;
+    }
+
+    public float distanceOfHexagons;
+    public GameObject hexagon;
+    private int countY;
+    private int countX;
+    private float cellSize;
+    private int[,] gridArray;
+
+    private void Awake()
+    {
+        distanceOfHexagons = 0.025f;
+    }
+
+    private void Start()
+    {
+        CreateGrid(9, 8, .5f);
+    }
+
+    public void CreateGrid(int countX, int countY, float cellSize)
+    {
+        this.countY = countY;
+        this.countX = countX;
+        this.cellSize = cellSize;
+
+        gridArray = new int[countY, countX];
+
+        bool showDebug = true;
+        if (showDebug)
+        {
+            TextMesh[,] debugTextArray = new TextMesh[countY, countX];
+
+            for (int x = 0; x < gridArray.GetLength(0); x++)
+            {
+                for (int y = 0; y < gridArray.GetLength(1); y++)
+                {
+                    if (x % 2 == 0)
+                    {
+                        GameObject obj = Instantiate(hexagon, GetWorldPosition(x, y) + new Vector3(cellSize * -.125f * x + distanceOfHexagons * x, cellSize * .5f * 2f + distanceOfHexagons * y), Quaternion.identity, transform);
+                        obj.transform.localScale *= cellSize;
+                        Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                        Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+                    }
+                    else
+                    {
+                        GameObject obj = Instantiate(hexagon, GetWorldPosition(x, y) + new Vector3(cellSize * -.125f * x + distanceOfHexagons * x, cellSize * .5f + distanceOfHexagons * y), Quaternion.identity, transform);
+                        obj.transform.localScale *= cellSize;
+                        Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                        Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+                    }
+                }
+            }
+            Debug.DrawLine(GetWorldPosition(0, countX), GetWorldPosition(countY, countX), Color.white, 100f);
+            Debug.DrawLine(GetWorldPosition(countY, 0), GetWorldPosition(countY, countX), Color.white, 100f);
+
+            OnGridValueChanged += (object sender, OnGridValueChangedEventArgs eventArgs) =>
+            {
+                debugTextArray[eventArgs.x, eventArgs.y].text = gridArray[eventArgs.x, eventArgs.y].ToString();
+            };
+        }
+    }
+
+    public int GetWidth()
+    {
+        return countY;
+    }
+
+    public int GetHeight()
+    {
+        return countX;
+    }
+
+    public float GetCellSize()
+    {
+        return cellSize;
+    }
+
+    public Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * cellSize + transform.position;
+    }
+
+    private void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
+        x = Mathf.FloorToInt((worldPosition - transform.position).x / cellSize);
+        y = Mathf.FloorToInt((worldPosition - transform.position).y / cellSize);
+    }
+
+    public void SetValue(int x, int y, int value)
+    {
+        if (x >= 0 && y >= 0 && x < countY && y < countX)
+        {
+            gridArray[x, y] = value;
+            if (OnGridValueChanged != null) OnGridValueChanged(this, new OnGridValueChangedEventArgs { x = x, y = y });
+        }
+    }
+
+    public void SetValue(Vector3 worldPosition, int value)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetValue(x, y, value);
+    }
+
+    public int GetValue(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < countY && y < countX)
+        {
+            return gridArray[x, y];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public int GetValue(Vector3 worldPosition)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        return GetValue(x, y);
+    }
+}
